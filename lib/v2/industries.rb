9f145063de289a2a52cf8dee2c53e82d32f61d64
Daniel Nelson <daniel.nelson@dealfront.com>
2024-08-08 08:10:15 -0600

    creating a gem

---
+++ lib/v2/industries.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "industries/version"
+
+module V2
+  module Industries
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
